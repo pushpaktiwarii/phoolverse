40675578146c134a1b1489e034ccd37c49fd0a72
Pushpak Tiwari <pushpaktiwari6@gmail.com>
2026-01-16 23:31:15 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Replace with your Computer's Local IP Address found via 'ipconfig' (e.g., 192.168.1.5)
 // Do NOT use 'localhost' if testing on a physical phone.
 // Use '10.0.2.2' if testing on Android Emulator.
-export const SOCKET_URL = "http://192.168.1.3:3000"; // Updated to detected IP from Metro logs
+export const SOCKET_URL = "https://phoolverse.onrender.com"; // Updated to detected IP from Metro logs
