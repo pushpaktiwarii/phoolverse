08763c3911bef0f57030c176c9f13b2bfcd71337
Pushpak Tiwari <pushpaktiwari6@gmail.com>
2026-01-19 23:49:12 +0530

    Create firebaseConfig.js

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBd0ylFwj-t9EK15osfQb4oL44bIu0VLKw",
+    authDomain: "foolverse.firebaseapp.com",
+    projectId: "foolverse",
+    storageBucket: "foolverse.firebasestorage.app",
+    messagingSenderId: "446483566416",
+    appId: "1:446483566416:web:bccfe897222d2ac739a655",
+    measurementId: "G-R5PM1LLXJ9",
+    databaseURL: "https://foolverse-default-rtdb.firebaseio.com"
+};
