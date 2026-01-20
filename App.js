63f1d8576eb841e51fe98443bce753c485899eba
Pushpak Tiwari <pushpaktiwari6@gmail.com>
2026-01-20 22:58:45 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import * as Notifications from 'expo-notifications';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import AppNavigator from './src/navigation/AppNavigator';
 
 // Configure Notifications to show even when app is in foreground
@@ -18,6 +19,13 @@ try {
 }
 
 export default function App() {
+  useEffect(() => {
+    async function lockPortrait() {
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+    }
+    lockPortrait();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <StatusBar style="light" />
